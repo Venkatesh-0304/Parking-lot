b38c635cd0c60572633c67c1f333c5c168887443
Venkatesh <venkatesh.t0304@gmail.com>
2026-05-26 11:26:21 +0530

    Created Ticket class and initialized

---
+++ ticket.rb
@@ -0,0 +1,16 @@
+class Ticket
+  attr_accessor :license_plate_number, :spot_no, :entry_time, :status
+  def initialize(license_plate_number, spot_no)
+    time = Time.now
+    @license_plate_number = license_plate_number
+    @spot_no = spot_no
+    @status = true
+    @entry_time = time.strftime("%d-%m-%y %H:%M")
+  end
+end
+
+t1 = Ticket.new("ka-05-ju-0376", 1)
+puts t1.license_plate_number
+puts t1.spot_no
+puts t1.entry_time
+puts t1.status
