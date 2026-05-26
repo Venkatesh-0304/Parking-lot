baeb4d629792a894ea54d26019d063768ef66af1
Venkatesh <venkatesh.t0304@gmail.com>
2026-05-26 12:32:35 +0530

    park_vehicle and find_vehicle method added

--- parking-lot.rb
+++ parking-lot.rb
@@ -1,5 +1,6 @@
 require_relative "vehicle"
 require_relative "parking-spot"
+require_relative "ticket"
 class ParkingLot
   attr_accessor :name, :spots, :vehichles, :tickets
   def initialize(name)
@@ -14,12 +15,35 @@ class ParkingLot
     @spots << spot
   end
 
-  def show_spots
-    @spots.each do |s|
-      puts "#{s.spot_no} "
-    end
+  def find_available_spot
+    available_spot = @spots.find { |s| s.available?}
   end
 
+  # def show_spots
+  #   @spots.each do |s|
+  #     puts "#{s.spot_no} #{s.available?}"
+  #   end
+  # end
+
+  def find_vehicle(license_plate_number)
+    @vehicles.find {|v| v.license_plate_number == license_plate_number}
+  end
+
+  def park_vehicle(license_plate_number)
+    if find_vehicle(license_plate_number) != nil
+      puts "Cannot park vehicle which is already Parked #{license_plate_number}"
+    else
+      spot = find_available_spot
+      if spot != nil
+        vehicle = Vehicle.new(license_plate_number)
+        @vehicles << vehicle
+        ticket = Ticket.new(vehicle, spot)
+        @tickets << ticket
+        spot.park!
+        puts "#{license_plate_number} parked successfully"
+      end
+    end
+  end
 end
 
 pl1 = ParkingLot.new("Garuda")
@@ -27,4 +51,4 @@ pl1 = ParkingLot.new("Garuda")
 puts pl1.name
 pl1.add_spot(2)
 pl1.add_spot(3)
-pl1.show_spots
+pl1.park_vehicle("ka-o5-ju-0376")
--- ticket.rb
+++ ticket.rb
@@ -1,9 +1,11 @@
+# require_relative "vehicle"
+# require_relative "parking-spot"
 class Ticket
   attr_accessor :license_plate_number, :spot_no, :entry_time, :status
-  def initialize(license_plate_number, spot_no)
+  def initialize(vehicle, spot)
     time = Time.now
-    @license_plate_number = license_plate_number
-    @spot_no = spot_no
+    @license_plate_number = vehicle.license_plate_number
+    @spot_no = spot.spot_no
     @status = true
     @entry_time = time.strftime("%d-%m-%y %H:%M")
   end
@@ -11,10 +13,12 @@ class Ticket
   def status?
     @status
   end
+  
 end
-
-t1 = Ticket.new("ka-05-ju-0376", 1)
-puts t1.license_plate_number
-puts t1.spot_no
-puts t1.entry_time
-puts t1.status
+# s1 = ParkingSpot.new(1)
+# v1 = Vehicle.new("ka-05-ju-0376")
+# t1 = Ticket.new(v1, s1)
+# puts t1.license_plate_number
+# puts t1.spot_no
+# puts t1.entry_time
+# puts t1.status
