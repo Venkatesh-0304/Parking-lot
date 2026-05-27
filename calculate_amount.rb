131fc2acce0593582cab982e2bab25ae4f87e24f
bhoomi456 <bhoomij0304@gmail.com>
2026-05-27 15:27:23 +0530

    calculate amount based on vehicle type

--- calculate_amount.rb
+++ calculate_amount.rb
@@ -1,6 +1,12 @@
 module CalculateAmount
-  PRICE_PER_HOUR = 20
-  def calculate_amount(duration)
-    puts "Total amount is #{duration * PRICE_PER_HOUR}"
+  PRICE_PER_HOUR_COMPACT = 20
+  PRICE_PER_HOUR_REGULAR = 30 
+
+  def calculate_amount(duration,spot_type)
+    if spot_type == "compact"
+      puts "Total amount is : #{duration * PRICE_PER_HOUR_COMPACT}"
+    elsif spot_type == "regular"
+      puts "Total amount is : #{duration * PRICE_PER_HOUR_REGULAR}"
+    end
   end
 end
