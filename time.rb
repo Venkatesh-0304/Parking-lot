75931485414cf9b89dfcd06abe965748b2256aac
Venkatesh-0304 <156488647+Venkatesh-0304@users.noreply.github.com>
2026-05-27 16:21:35 +0530

    git conflict resolved 1

--- time.rb
+++ time.rb
@@ -7,10 +7,6 @@ module CalculateTime
     duration_seconds = exit - entry
     hours = (duration_seconds / 3600).to_i
     minutes = (((duration_seconds / 3600) * 60)% 60).to_i
-<<<<<<< HEAD
     return "#{hours}.#{minutes}"
-=======
-    return hours,minutes
->>>>>>> 7bfc6dbc4476bf7134f26753efa6511af462543a
   end
 end
